321ea47f05d8cc9d96650448eec31dc56a220ec3
_Denmeltop_ Мельник <denmeltop228@gmail.com>
2023-01-12 01:20:57 +0200

    что-то обновил

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using Newtonsoft.Json;
+
+namespace MyMessanger
+{
+    class Progaram
+    {
+        static void Main(string[] args)
+        {
+            Message msg = new Message("RusAl", "Privet", DateTime.UtcNow);
+            string output = JsonConvert.SerializeObject(msg);
+            Console.WriteLine(output);
+            Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(deserializedMsg);
+            //{ "UserName":"RusAl","MessageText":"Privet","TimeStamp":"2023-01-11T23:18:15.7437725Z"}
+            //RusAl < 11.01.2023 23:18:15 >: Privet
+        }
+    }
+}
